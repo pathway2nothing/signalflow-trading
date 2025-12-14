8c3c6b3e55acd2252f2eaabcc5e29221aeaae682
Path Way <pathwat2nothing@gmail.com>
2025-12-14 00:06:53 +0000

    refactor core module structure and add component registry system

--- src/signalflow/core/__init__.py
+++ src/signalflow/core/__init__.py
@@ -1,10 +1,16 @@
-from .data_containers import RawData, Signals
-from .enums import SignalType, PositionType
+from .containers import RawData, Signals
+from .enums import SignalType, PositionType, SfComponentType
+from .decorators import sf_component
+from .registry import default_registry, SignalFlowRegistry
 
 
 __all__ = [
     "RawData", 
     "Signals", 
     "SignalType",
-    "PositionType"
+    "PositionType",
+    "SfComponentType",
+    "sf_component",
+    "default_registry",
+    "SignalFlowRegistry"
 ]
