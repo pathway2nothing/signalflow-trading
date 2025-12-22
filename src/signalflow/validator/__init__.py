196184dffb2bb8c2c21cc398d061162832a96615
Path Way <pathwat2nothing@gmail.com>
2025-12-22 21:48:16 +0000

    feat: add new signal validation framework with Sklearn and Lightning validators, enhance requirements with additional libraries

--- src/signalflow/validator/__init__.py
+++ src/signalflow/validator/__init__.py
@@ -1,5 +1,11 @@
-from signalflow.validator.validator import SignalValidator
+from signalflow.validator.base_signal_validator import SignalValidator
+from signalflow.validator.sklearn_validator import SklearnSignalValidator
+from signalflow.validator.lightning_temporal_validator import LightningTemporalValidator
+from signalflow.validator.lightning_tabular_validator import LightningTabularValidator
 
 __all__ = [
     "SignalValidator",
+    "SklearnSignalValidator",
+    "LightningTemporalValidator",
+    "LightningTabularValidator",
 ]
